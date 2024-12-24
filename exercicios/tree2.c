212feaf6b1272b8bc95e99048660659071b7b535
Professor José de Assis <josedeassisfilho@gmail.com>
2024-12-24 19:34:00 -0300

    Add files via upload

---
+++ exercicios/tree2.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+
+/* 
+	Professor José de Assis
+	Curso básico de Linguagem C
+	Estudo do laço for
+	Desenhando uma árvore de natal
+	Versão 2.0
+*/
+
+int i,j,star,tamanho;
+
+//função para modificar a cor do texto
+void mudarCor(int cor) {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),cor);
+}
+
+int main(int argc, char *argv[]) {
+	//iniciar o gerador de números aleatórios
+	srand(time(NULL));
+	printf("Feliz Natal!\n");
+	printf("Tamanho: ");
+	scanf("%d",&tamanho);
+	printf("\n");
+	//Laço principal que define o tamanho da árvore
+	for (i=1; i<=tamanho; i++) {
+		//laço que define o espaçamento
+		for (j=1; j<=tamanho-i; j++) {
+			printf(" ");
+		}
+		//laço que desenha a árvore
+		for (star=1; star<=i*2-1; star++) {
+			//sorteio de 5 números (0 1 2 3 4)
+			if (rand()%5 == 0) {
+				int cor = (rand()%15)+1; // 1...15
+				mudarCor(cor);
+				printf("o");
+			} else {
+				mudarCor(10);
+				printf("*");
+			}
+		}
+		printf("\n");
+	}
+	//Laço que desenha o tronco da árvore
+	for (i=1; i<=2; i++) {
+		for (j=1; j<=tamanho-1; j++) {
+			printf(" ");
+		}
+		mudarCor(6);
+		printf("|");
+		printf("\n");
+	}
+	mudarCor(7);	
+	printf("\n");
+	system("pause");
+	return 0;
+}
